9c1525fc44650e75cc683f5a0c740a28bfcabbdd
JamboAthletic <>
2022-03-01 19:24:53 +0100

    dsa

--- mail.php
+++ mail.php
@@ -4,6 +4,7 @@
       //   // $apiKey = getenv('SENDGRID_API_KEY');
       //   // $sg = new SendGrid($apiKey);
       //   $email = new \SendGrid\Mail\Mail();
+      echo "Message was send";
       $header = "From: twoj@email.com \nContent-Type:".
           ' text/plain;charset="UTF-8"'.
           "\nContent-Transfer-Encoding: 8bit";
