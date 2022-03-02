d6128b1992b8f29d6c77fa1e24a9b6687b5f1ee0
JamboAthletic <>
2022-03-02 10:45:04 +0100

    maik

--- mail.php
+++ mail.php
@@ -1,15 +1,13 @@
 <?php
-      // define('__ROOT__', dirname(dirname(__FILE__)));
-      //   require('./packages/sendgrid-php-main/sendgrid-php.php');
-      //   // $apiKey = getenv('SENDGRID_API_KEY');
-      //   // $sg = new SendGrid($apiKey);
-      //   $email = new \SendGrid\Mail\Mail();
-      echo "Message was send";
-      $header = "From: twoj@email.com \nContent-Type:".
-          ' text/plain;charset="UTF-8"'.
-          "\nContent-Transfer-Encoding: 8bit";
-      $to = "snikersdro@gmail.com";
-      $subject = "Wiadomość testowa";
-      $message = "Witaj to wiadomość testowa";
-      mail($to, $subject, $message, $header);
+    ini_set("SMTP","smtp.gmail.com" );
+    ini_set("smtp_port","587");
+    ini_set('sendmail_from', 'snikersdro@gmail.com');   
+    echo "Message was send";
+    $header = "From: twoj@email.com \nContent-Type:".
+        ' text/plain;charset="UTF-8"'.
+        "\nContent-Transfer-Encoding: 8bit";
+    $to = "snikersdro@gmail.com";
+    $subject = "Wiadomość testowa";
+    $message = "Witaj to wiadomość testowa";
+    mail($to, $subject, $message, $header);
 ?>
